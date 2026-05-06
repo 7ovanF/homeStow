bd48fa45173446cff7ec18d1b9f77f8aaedd8b77
Stanislaw Zimermann <jovanfinesta@gmail.com>
2026-05-06 09:46:20 +0700

    im addicted to ricing

--- dotfiles/nvim/.config/nvim/lua/config/options.lua
+++ dotfiles/nvim/.config/nvim/lua/config/options.lua
@@ -10,3 +10,6 @@ vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
 vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.opt.autoindent = true -- Enable automatic indentation
+
+-- Extra Controls
+vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true })
