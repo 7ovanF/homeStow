4bb5218fc0dbaf5753b45f01cbd2f5e2f34fe208
Stanislaw Zimermann <jovanfinesta@gmail.com>
2026-05-06 09:18:31 +0700

    more waybar config

--- dotfiles/waybar/.config/waybar/statusbar/date-time.sh
+++ dotfiles/waybar/.config/waybar/statusbar/date-time.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 DaySuffix() {
-  case `date +%-d` in
-    1|21|31) echo "st";;
-    2|22)    echo "nd";;
-    3|23)    echo "rd";;
-    *)       echo "th";;
-  esac
+    case $(date +%-d) in
+    1 | 21 | 31) echo "st" ;;
+    2 | 22) echo "nd" ;;
+    3 | 23) echo "rd" ;;
+    *) echo "th" ;;
+    esac
 }
-date=$(date "+%A %-d`DaySuffix` %B %Y - %H:%M")
+# `DaySuffix`gone lmao
+date=$(date "+%A, %-d %B %Y - %H:%M")
 icon=""
 printf "%s" "$date"
