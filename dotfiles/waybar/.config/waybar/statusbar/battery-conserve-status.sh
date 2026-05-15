01d8e45f2382ec7accd95b8236add64b041a071a
Stanislaw Zimermann <jovanfinesta@gmail.com>
2026-05-15 16:00:39 +0700

    plymouth ideapad logo theme

--- dotfiles/waybar/.config/waybar/statusbar/battery-conserve-status.sh
+++ dotfiles/waybar/.config/waybar/statusbar/battery-conserve-status.sh
@@ -5,7 +5,7 @@ PATH_SYS="/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
 current=$(cat "$PATH_SYS")
 
 if [ "$current" = "1" ]; then
-    printf '{"text":"","class":"on","tooltip":"Battery Conservation Enabled!"}\n'
+    printf '{"text":"󰒙","class":"on","tooltip":"Battery Conservation Enabled!"}\n'
 else
     printf '{"text":"","class":"off","tooltip":"Battery Conservation Disabled."}\n'
 fi
